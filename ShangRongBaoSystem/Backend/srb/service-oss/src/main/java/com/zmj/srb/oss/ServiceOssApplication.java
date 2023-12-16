74dc801e928d446e232d7aa827cccf02b1bb37ca
dream98job@126.com <dream98job@126.com>
2023-12-16 20:35:13 +0800

    阿里云文件管理

---
+++ ShangRongBaoSystem/Backend/srb/service-oss/src/main/java/com/zmj/srb/oss/ServiceOssApplication.java
@@ -0,0 +1,14 @@
+package com.zmj.srb.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.zmj.srb"})
+public class ServiceOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOssApplication.class, args);
+    }
+}
---
+++ ShangRongBaoSystem/Backend/srb/service-oss/src/test/java/com/zmj/srb/oss/OssTest.java
@@ -0,0 +1,31 @@
+package com.zmj.srb.oss;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class OssTest {
+
+    @Test
+    public void create(){
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+
+// 从环境变量中获取访问凭证。运行本代码示例之前，请先配置环境变量。
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, credentialsProvider);
+        System.out.println(ossClient);
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+    }
+}
